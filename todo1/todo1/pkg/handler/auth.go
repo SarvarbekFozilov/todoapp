e6900cbc6dff76731fe3349b351f95648b17c8bc
SarvarbekFozilov <152473189+SarvarbekFozilov@users.noreply.github.com>
2023-12-13 01:06:24 +0000

    little updated

---
+++ todo1/todo1/pkg/handler/auth.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"net/http"
+	models "todo/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) signUp(c *gin.Context) {
+
+	var user models.CreateUserReq
+
+	if err := c.BindJSON(&user); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	id, err := h.services.Authorization.CreateUser(&user)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+
+}
+
+
+
+func (h *Handler) signIn(c *gin.Context) {
+	var input models.SignIn
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
+}
